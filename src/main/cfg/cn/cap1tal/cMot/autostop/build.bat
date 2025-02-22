ef31c81b8a3f78310b507a1518f764649177efa8
tomh500 <tomh500@163.com>
2025-02-22 13:53:05 +0800

    1.3

--- src/main/cfg/cn/cap1tal/cMot/autostop/build.bat
+++
@@ -1,4 +0,0 @@
-g++ calcautostop.cpp -o calcautostop.exe -std=c++23 -O2 -finput-charset=UTF-8 -static
-g++ calcspeed.cpp -o calcspeed.exe -std=c++23 -O2 -finput-charset=UTF-8 -static
-g++ calc.cpp -o calc.exe -std=c++23 -O2 -finput-charset=UTF-8 -static
-echo Completed
--- src/main/cfg/cn/cap1tal/cMot/autostop/calc.bat
+++
@@ -1,2 +0,0 @@
-
-g++ calc.cpp -o calc.exe -std=c++23 -O2 -finput-charset=UTF-8 -static
